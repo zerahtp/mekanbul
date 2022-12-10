536d31c2ba7448dc95ca691a94877cad986b3086
zerahtp <hatipogluuzehra@gmail.com>
2022-12-10 14:13:02 +0300

    son

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 var createError = require('http-errors');
-require("./app_api/models/db");
-var express=require('express');
-
+var express = require('express');
+var session=require('express-session');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('./app_api/models/db');
 var apiRouter=require("./app_api/routes/index");
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 
 var app = express();
-// view engine setup
-app.set('views', path.join(__dirname,'app_server','views'));
-app.set('view engine', 'pug');
 
+// view engine setup
+app.set('views', path.join(__dirname,'app_server', 'views'));
+app.set('view engine', 'pug');
+app.use(session({
+  secret:'gizli',
+  cookie:{maxAge:1000*60*60*24},
+  resave:true,
+  saveUninitialized:true
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,6 +1,6 @@
 var mongoose=require("mongoose");
-var dbURI = 'mongodb://localhost/mekanbul'; 
-//var dbURI="mongodb+srv://zerahtp:mekanbul123@mekanbul.vzr6zbb.mongodb.net/mekanbul?retryWrites=true&w=majority";
+//var dbURI = 'mongodb://localhost/mekanbul'; 
+var dbURI="mongodb+srv://zerahtp:mekanbul123@mekanbul.vzr6zbb.mongodb.net/mekanbul?retryWrites=true&w=majority";
 
 mongoose.connect(dbURI);
 function kapat(msg,callback){
